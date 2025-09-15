3eca10102aab6852ab27880870336aebc26e6b5d
MarikaRomeo <marika.romeo.syne25lin@edu.tucsweden.se>
2025-09-15 11:25:36 +0200

    Add project files.

---
+++ EnkelMiniräknare/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EnkelMiniräknare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, välja en operation");
+        }
+    }
+}
